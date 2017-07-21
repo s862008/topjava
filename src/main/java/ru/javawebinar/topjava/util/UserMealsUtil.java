06d992d437102df94a8d19c687d3aed8bebbc4ce
Admin <s862008@yandex.ru>
2017-07-21 18:02:15 +0300

    HW0_2107

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -7,10 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 
 /**
@@ -22,7 +19,7 @@ public class UserMealsUtil {
         List<UserMeal> mealList = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 20,20,0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
@@ -67,7 +64,52 @@ public class UserMealsUtil {
               // to do
         }
 
-        mapList.forEach((k,v) -> System.out.println("key: "+k + " val: "+v));
+      //  mapList.forEach((k,v) -> System.out.println("key: "+k + " val: "+v));
+        Map<LocalDate,Integer> mapList2  = new HashMap<>();
+        Collections.sort(mealList,(left, right) -> left.getDateTime().compareTo(right.getDateTime()));
+        mealList.forEach((k) -> System.out.println(k.getDateTime()+" "+k.getDescription()+" " +k.getCalories()));
+        Integer summ =0;
+        for(int i=0; i < mealList.size();i++){
+            if(i == 0){
+                tmp = mealList.get(i).getDateTime().toLocalDate();
+                summ = mealList.get(i).getCalories();
+                mapList2.put(tmp,summ);
+                i++;
+            }
+            LocalDate t = mealList.get(i).getDateTime().toLocalDate();
+            if(t.equals(tmp)){
+                summ = mapList2.get(tmp) + mealList.get(i).getCalories() ;
+                mapList2.put(tmp,summ);
+            }else {
+                try{
+                    if(!mapList.get(t).isEmpty()) {
+                        summ = mapList2.get(t) + mealList.get(i).getCalories();
+                        mapList2.put(t,summ);
+
+                    }
+                } catch(NullPointerException ex){
+                    summ =  mealList.get(i).getCalories();
+                    mapList2.put(t, summ);
+                }
+
+                tmp = t;
+            }
+        }
+        mapList2.forEach((k,v) -> System.out.println("key: "+k + " val: "+v));
+        List<UserMealWithExceed> listUMEx = new ArrayList<>();
+        for(UserMeal um : mealList){
+            boolean ex =true;
+            if( mapList2.get(um.getDateTime().toLocalDate()) <= caloriesPerDay )
+            { ex = false;}
+            listUMEx.add(new UserMealWithExceed(
+                    um.getDateTime(),
+                    um.getDescription(),
+                    um.getCalories(),
+                    ex)
+            ) ;
+
+        }
+       listUMEx.forEach(v -> System.out.println(v.getDateTime()+" "+v.getDescription()+" "+v.getDateTime()));
        // System.out.println(mapList.get(0));
 
         /*for (UserMeal meal : mealList) {
